ec15ea74bc245fb3aa78e131da7005ebdd17a40f
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2021-11-23 18:47:41 +0100

    form changes

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { Button } from './Styles/Components.style'
 import { InvoiceContext } from '../Context/InvoiceContext'
+import TrashIcon from '../assets/icon-delete.svg'
 
 const FormItem = ({name, quantity, price, total, setItemValue, onTotalVariablesChange}) => {
 
@@ -174,26 +176,36 @@ const Form = ({invoice, setFormOpen}) => {
     // return (
         <>
             {/* Invoice Form */}
+        <FormWrapper>
+            <FormMainWrapper>
             <div>
                 <Link to="/">
                     <div>{"<"} Go back</div>
                 </Link>
                 <h2>{ invoice.id ? `Edit ${invoice.id}` : "New Invoice" }</h2>
                 <form action="">
-                    <div>Bill From</div>
+                    <h4>Bill From</h4>
                     <div>
                         <label htmlFor="street-address-from">Street Address</label>
                         <input type="text" value={senderAddress.street} onChange={e => handleSAChange(e)} name="street" id="street-address-from" />
                     </div>
-                    <div>
-                        <label htmlFor="city">City</label>
-                        <input type="text" name="city" value={senderAddress.city} onChange={e => handleSAChange(e)} />
-                        <label htmlFor="post-code">Post Code</label>
-                        <input type="text" name="postCode" value={senderAddress.postCode} onChange={e => handleSAChange(e)} />
-                        <label htmlFor="country">Country</label>
-                        <input type="text" name="country" value={senderAddress.country} onChange={e => handleSAChange(e)}  />
+                    <div className="formBillPlace">
+                        <div className="formBillPlaceCityPost">
+                            <div>
+                                <label htmlFor="city">City</label>
+                                <input type="text" name="city" value={senderAddress.city} onChange={e => handleSAChange(e)} />
+                            </div>
+                            <div>
+                                <label htmlFor="post-code">Post Code</label>
+                                <input type="text" name="postCode" value={senderAddress.postCode} onChange={e => handleSAChange(e)} />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="country">Country</label>
+                            <input type="text" name="country" value={senderAddress.country} onChange={e => handleSAChange(e)}  />
+                        </div>
                     </div>
-                    <div>Bill To</div>
+                    <h4>Bill To</h4>
                     <div>
                         <label htmlFor="clietns-name">Client’s Name</label>
                         <input type="text" name="clientName" value={formData.clientName} onChange={e => handleFormDataChange(e)} />
@@ -202,13 +214,21 @@ const Form = ({invoice, setFormOpen}) => {
                         <label htmlFor="street-address">Street Address</label>
                         <input type="text" name="street" value={clientAddress.street} onChange={e => handleCAChange(e)} />
                     </div>
-                    <div>
-                        <label htmlFor="client-city">City</label>
-                        <input type="text" name="city" value={clientAddress.city} onChange={e => handleCAChange(e)} />
-                        <label htmlFor="client-post-code">Post Code</label>
-                        <input type="text" name="postCode" value={clientAddress.postCode} onChange={e => handleCAChange(e)} />
-                        <label htmlFor="country">Street Address</label>
-                        <input type="text" name="country" value={clientAddress.country} onChange={e => handleCAChange(e)} />
+                    <div className="formBillPlace">
+                        <div className="formBillPlaceCityPost">
+                            <div>
+                                <label htmlFor="client-city">City</label>
+                                <input type="text" name="city" value={clientAddress.city} onChange={e => handleCAChange(e)} />
+                            </div>
+                            <div>
+                                <label htmlFor="client-post-code">Post Code</label>
+                                <input type="text" name="postCode" value={clientAddress.postCode} onChange={e => handleCAChange(e)} />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="country">Street Address</label>
+                            <input type="text" name="country" value={clientAddress.country} onChange={e => handleCAChange(e)} />
+                        </div>
                     </div>
                     <div>
                         <label htmlFor="client-city">Invoice Date</label>
@@ -237,27 +257,34 @@ const Form = ({invoice, setFormOpen}) => {
             </div>
             {/* Items Form */}
             <div>
+
+                <h3>Item List</h3>
                 {items && items.map(item => (
-                    <div key={item.id}>
+                    <div className="formAddNewItem" key={item.id}>
                         <FormItem key={item.id} {...item} setItemValue={e => setItemValue(e, item.id)} onTotalVariablesChange={onTotalVariablesChange} />
-                        <Button type="delete" onClick={() => deleteItem(item.id)}>delete item</Button>
+                        <Button className="formTrashBtn" onClick={() => deleteItem(item.id)}>
+                            <img src={TrashIcon} alt="delete-icon" />
+                        </Button>
+                        <div>
+                            <label htmlFor="">
+                                <input type="total" value={totalState} readOnly />
+                            </label>
+                        </div>
                     </div>
                 ))}
             </div>
             <Button onClick={addItem} type="new-item">Add Item</Button>
-            <div>
-                <label htmlFor="">
-                    <input type="total" value={totalState} readOnly />
-                </label>
-            </div>
-            <div>
-                <Button onClick={e => setFormOpen(false)}>
+            </FormMainWrapper>
+            
+            <FormButtonWrapper>
+                <Button type="draft" onClick={e => setFormOpen(false)}>
                     Cancel
                 </Button>
-                <Button onClick={saveInvoice}>
+                <Button type="purple" onClick={saveInvoice}>
                     Save Changes
                 </Button>
-            </div>
+            </FormButtonWrapper>
+        </FormWrapper> 
         </>
         , document.getElementById("portal"))
 }
@@ -267,5 +294,117 @@ const Form = ({invoice, setFormOpen}) => {
         // </div>
     // )
 // }
+
+const FormWrapper = styled.div ` 
+    position: fixed;
+    min-height: 100vh;
+    width: 100%;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: ${props => props.theme.color.form.bg};
+    z-index: 5;
+    overflow: auto;
+    @media screen and (min-width: 768px) {
+            max-width: 616px;
+            right: auto;
+        }
+
+`
+const FormButtonWrapper = styled.div ` 
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 21px 24px;
+    gap: 8px;
+    background-color: ${props => props.theme.color.invoiceItem.bg};
+`
+
+const FormMainWrapper = styled.div ` 
+    padding: 32px 24px 0;
+
+    h2 {
+        color: ${props => props.theme.color.text.heading}; 
+        margin: 24px 0;
+    }
+    h3 {
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 32px;
+        letter-spacing: -0.375px;
+        color: #777F98;
+        margin-bottom: 24px;
+    }
+    h4 {
+        color: #7C5DFA;
+        font-weight: 700;
+        font-size: 12px;
+        line-height: 15px;
+        margin-bottom: 24px;
+    }
+
+    label {
+        font-weight: 500;
+        font-size: 12px;
+        line-height: 15px;
+        color: ${props => props.theme.color.text.formLabel}; 
+        letter-spacing: -0.25px;
+        display: block;
+        margin-bottom: 10px;
+    }
+    input {
+        width: 100%;
+        /* border: 1px solid ${props => props.valid ? props.theme.color.form.fieldBorder : '#EC5757'}; */
+        border: 1px solid ${props => props.theme.color.form.fieldBorder};
+        border-radius: 4px;
+        padding: 16px;
+        margin-bottom: 24px;
+        background: ${props => props.theme.color.form.fieldBg};
+        outline: none;
+        color: ${props => props.theme.color.text.heading};
+        font-weight: bold;
+        transition: all 0.3s ease;
+        ::placeholder {
+            color: ${props => props.theme.color.text.placeholder};
+            transition: color .3s;
+        }
+        :focus {
+            border: 1px solid #9277FF;
+        }
+    }
+
+    .formBillPlace {
+
+        @media screen and (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 24px;
+            width: 100%;
+        }
+    }
+
+    .formBillPlaceCityPost {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 23px;
+        /* @media screen and (min-width: 768px) {
+
+            justify-content: space-between;
+            flex: 1;
+        } */
+    }
+    .formTrashBtn {
+        background: transparent;
+    }
+    .formAddNewItem {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+`
 
 export default Form
